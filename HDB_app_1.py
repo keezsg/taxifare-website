1a95feea938ea91b3000ca39a45279c8f60d339a
boonkee tan <keez79@gmail.com>
2025-04-13 23:01:50 +0800

    added HDB app and chart temporary

---
+++ HDB_app_1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+
+
+st.title("HDB buying & selling companion")
+
+st.header("Welcome to HDB chatbot")
